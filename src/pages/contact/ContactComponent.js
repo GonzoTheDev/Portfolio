ceb945eafe3efa53ab24e9f810a2ea3aa3db7bd2
GonzoTheDev <c20703429@mytudublin.ie>
2024-04-21 07:58:53 +0100

    Update ContactComponent.js

--- src/pages/contact/ContactComponent.js
+++ src/pages/contact/ContactComponent.js
@@ -57,7 +57,7 @@ function Contact(props) {
           </div>
         </Fade>
         <Fade bottom duration={1000} distance="40px">
-          <div className="blog-heading-div">
+          /*<div className="blog-heading-div">
             <div className="blog-heading-text-div">
               <h1 className="blog-heading-text" style={{ color: theme.text }}>
                 {blogSection["title"]}
@@ -77,7 +77,7 @@ function Contact(props) {
             <div className="blog-heading-img-div">
               <BlogsImg theme={theme} />
             </div>
-          </div>
+          </div>*/
         </Fade>
       </div>
       <Footer theme={props.theme} onToggle={props.onToggle} />
